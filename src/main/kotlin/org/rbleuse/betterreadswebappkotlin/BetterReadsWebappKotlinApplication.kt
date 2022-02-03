405357939a70797fedfbd07788258ba56353f04b
Rémi Bleuse <remi.bleuse@gmail.com>
2022-02-03 10:17:23 +0900

    setup auth

--- src/main/kotlin/org/rbleuse/betterreadswebappkotlin/BetterReadsWebappKotlinApplication.kt
+++ src/main/kotlin/org/rbleuse/betterreadswebappkotlin/BetterReadsWebappKotlinApplication.kt
@@ -2,9 +2,21 @@ package org.rbleuse.betterreadswebappkotlin
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.security.core.annotation.AuthenticationPrincipal
+import org.springframework.security.oauth2.core.user.OAuth2User
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
 
 @SpringBootApplication
-class BetterReadsWebappKotlinApplication
+@RestController
+class BetterReadsWebappKotlinApplication {
+
+    @RequestMapping("/user")
+    fun user(@AuthenticationPrincipal principal: OAuth2User): String? {
+        println(principal)
+        return principal.getAttribute("name")
+    }
+}
 
 fun main(args: Array<String>) {
     runApplication<BetterReadsWebappKotlinApplication>(*args)
---
+++ src/main/kotlin/org/rbleuse/betterreadswebappkotlin/configuration/SecurityAdapter.kt
@@ -0,0 +1,24 @@
+package org.rbleuse.betterreadswebappkotlin.configuration
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.http.HttpStatus
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
+import org.springframework.security.web.authentication.HttpStatusEntryPoint
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository
+
+@Configuration
+class SecurityAdapter : WebSecurityConfigurerAdapter() {
+
+    @Throws(Exception::class)
+    override fun configure(http: HttpSecurity) {
+        http.csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+        http.logout().logoutSuccessUrl("/").permitAll()
+        http.oauth2Login()
+        http.authorizeRequests()
+            .antMatchers("/", "/error").permitAll()
+            .anyRequest().authenticated()
+
+        http.exceptionHandling().authenticationEntryPoint(HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED))
+    }
+}
